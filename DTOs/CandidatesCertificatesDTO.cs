59b4077de6832d89e89fbb4181863fb1d11c782c
Konstantinos_Pappas <154928208+Konstantinos-Pa@users.noreply.github.com>
2025-12-18 18:53:27 +0200

    Delete DTOs/CandidatesCertificatesDTO.cs

--- DTOs/CandidatesCertificatesDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using Assignment.Models;
-
-namespace Assignment.DTOs
-{
-    public class CandidatesCertificatesDTO
-    {
-        List<Certificate>? CandidatesCertificates { get; set; } = new List<Certificate>();
-    }
-}
